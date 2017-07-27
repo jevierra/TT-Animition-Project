50959f7fee4988c6cafea14d0e417c87a8120d4c
jeffrey vierra <st.pious@gmail.com>
2017-07-26 15:55:00 -1000

    Added transition code
    Also remove all the individual class links: and added:
    
    linkElement:'header a',
    
    Which  applies the class to all headers and anchor tags

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-$(".animsition").animsition();
+$(".animsition").animsition(
+	{
+
+		inClass: 'fade-in-right-lg',
+		outClass: 'fade-out-right-lg',
+		linkElement:'header a',
+		inDuration: 1000,
+		outDuration:500
+
+
+
+
+
+
+
+	});
