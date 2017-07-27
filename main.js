fc23b0a2c0ec74978c5102feec4af2554b98ce8b
jeffrey vierra <st.pious@gmail.com>
2017-07-27 07:36:18 -1000

    Sticky Js attached
    Modified the header class in css.

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 $(".animsition").animsition(
 	{
 
-		inClass: 'fade-in-right-lg',
+		/*inClass: 'fade-in-right-lg',
 		outClass: 'fade-out-right-lg',
 		linkElement:'header a',
 		inDuration: 1000,
 		outDuration:500
+*/
 
-
-
-
-
+    inClass: 'zoom-in-sm',
+    outClass: 'zoom-out-sm',
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: 'header a',
 
 
 	});
+
+
+
+
+
+$('.header').sticky();
